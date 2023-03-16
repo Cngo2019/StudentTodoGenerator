ed185d48201d66d0478dc4791c938c7d651f6488
Cngo2019 <canngo215@gmail.com>
2023-03-16 14:04:04 -0400

    fix infinite loop exception in main menu

--- src/com/studenttodogenerator/Application.java
+++ src/com/studenttodogenerator/Application.java
@@ -2,6 +2,7 @@ package com.studenttodogenerator;
 
 import com.studenttodogenerator.managers.EventManager;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Application {
@@ -12,22 +13,29 @@ public class Application {
 
         boolean userIsUsing = true;
         while (userIsUsing) {
-            eventManager.displayOptions();
-            System.out.println("Select which option you want by typing the number in the terminal and pressing enter: ");
-            int userOption = sc.nextInt();
-            sc.nextLine();
-            switch (userOption) {
-                case 1:
-                    eventManager.handleCreateTodoSheet();
-                    break;
-                case 2:
-                    // Read from the sheet
-                    eventManager.handleReadTodoSheet();
-                    break;
-                case 3:
-                    // Exit the program
-                    userIsUsing = false;
-                    break;
+            try {
+                eventManager.displayOptions();
+                System.out.println("Select which option you want by typing the number in the terminal and pressing enter: ");
+                int userOption = sc.nextInt();
+                sc.nextLine();
+                
+                switch (userOption) {
+                    case 1:
+                        eventManager.handleCreateTodoSheet();
+                        break;
+                    case 2:
+                        // Read from the sheet
+                        eventManager.handleReadTodoSheet();
+                        break;
+                    case 3:
+                        // Exit the program
+                        userIsUsing = false;
+                        break;
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("You entered an invalid input.");
+                sc.nextLine();
             }
         }
 
